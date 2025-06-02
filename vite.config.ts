3975e3fc06288db69aed2c8ebd89303d002f1206
jmdc-yhori <yhori@jmdc.co.jp>
2025-06-03 08:39:52 +0900

    add: homeコントローラーを追加してrootパスに設定,viteの設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    react()
   ],
+  server: {
+    host: '0.0.0.0',
+    port: 3036,
+    strictPort: true,
+    hmr: {
+      host: 'localhost',
+    }
+  },
 })
